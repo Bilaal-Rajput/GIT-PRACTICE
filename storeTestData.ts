bfec0629a512954ebff7118dd5b9832314d522c9
Bilaal Rajput <bilaalrajput381@gmail.com>
2024-11-22 02:36:02 -0800

    initial

---
+++ storeTestData.ts
@@ -0,0 +1,26 @@
+export const storeTestData={
+    cartText:" BSc (Hons) French First Lady - testing certificates -",
+    cartTitle:"Transcript",
+    cartQuantity:"1",
+}
+export const BillingAddressData={
+    firstName:"John",
+    lastName:"Doe",
+    address1:"123 Elm StreeDoe",
+    address2:"Apt 4B",
+    email:" johndoe@example.com",
+    city:"Springfield",
+    county:"Montgomery",
+    country:"United States",
+    postCode: "54321",
+    phone:"+1-555-123-4567",
+}
+
+export const documentDetailsTestData = {
+    noRecordMessage: "You have no records of this type"
+}
+
+export const basketTestData = {
+    editBtn: "Edit basket",
+    emptyBasketMessage: "There are no items in your basket"
+}
